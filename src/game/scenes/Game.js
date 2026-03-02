365a3612510369c760cd7caacebe4607e5df32eb
bryanquezada-sketch <bryan.quezada1@outlook.com>
2026-03-01 23:02:25 -0500

    Created Spawn Lanes

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -29,9 +29,21 @@ export class Game extends Scene
        this.fallingObjects.body.setCircle(16);
 //       this.fallingObjects.body.setAllowGravity(false);
 
-        
+       //Math.floor(...): Rounds the result down to the nearest whole integer. This ensures you do not have a "partial" lane if the screen width isn't perfectly divisible by 16.
+       this.totalLanes = Math.floor(this.scale.width / 16);
 
+       //Array.from({ length: ... }): Creates a new array with a specific number of empty slots based on totalLanes.
+       this.spawnLanes = Array.from({ length: this.totalLanes }, (v, i) => i * 16);
+       
+       /* (i * 16) + 8: Calculates the start of the lane (i * 16) and adds half of the lane width (8).
+       Logic: Most Phaser sprites use a center-origin (0.5). If you spawn a sprite at x = 0, half the sprite will be off-screen to the left. By adding 
+        8, the sprite’s center aligns with the middle of the 
+        16-pixel lane.
+        Result: this.spawnLanes becomes an array of center-positions: [8, 24, 40, 56, ...].
+       this.spawnLanes = Array.from({ length: this.totalLanes }, (v, i) => (i * 16) + 8);
+       */
 
+       
     
         this.cursors = this.input.keyboard.createCursorKeys();
         this.wasd = this.input.keyboard.addKeys ({
